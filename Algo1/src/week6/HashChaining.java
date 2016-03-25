8363f2968779b74309b0bdfac270a20efd353e18
prajesh <prajeshravindran@gmail.com>
2016-03-25 14:48:01 -0500

    hashing chaining

---
+++ Algo1/src/week6/HashChaining.java
@@ -0,0 +1,56 @@
+package week6;
+
+public class HashChaining<Key,Value> {
+  int M;
+  Node[] chain;
+  private class Node<Key,Value>{
+    Key key;
+    Value value;
+    Node next;
+    public Node(Key k, Value v)
+    {
+      this.key = k;
+      this.value = v;
+    }
+  }
+
+  public HashChaining(int m) {
+    this.M= m;
+    chain = new Node[this.M];
+  }
+  public void put(Key k, Value v)
+  {
+    Node node = new Node(k,v);
+    int index = k.hashCode()%this.M;
+    Node pos = chain[index];
+    if(pos==null)
+    {chain[index]=node;
+    return;}
+    
+    while(pos.next!=null)
+      pos = pos.next;
+    pos.next = node;
+  }
+  public Value get(Key k)
+  {
+    int index = k.hashCode()%this.M;
+    Node node = chain[index];
+    while(node!=null)
+      {
+        if(node.key==k) 
+          return (Value) node.value;
+        node = node.next;
+      }
+   return null;
+  }
+  public static void main(String[] args)
+  {
+    HashChaining<Integer,String> hc = new HashChaining<Integer,String>(2);
+    hc.put(1, "prajesh");
+    hc.put(2, "ashwin");
+    hc.put(3, "kill");
+    hc.put(7, "prakash");
+    System.out.println(hc.get(7));
+  }
+
+}
